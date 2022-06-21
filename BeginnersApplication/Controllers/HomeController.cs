2d20961ebe930b45e4457524349c12162c68de63
rajendren.Dhivya <rajendren.Dhivya@outlook.com>
2022-06-21 10:55:55 +0530

    Add MVC pattern for empty application

---
+++ BeginnersApplication/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeginnersApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        //  home/index
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            string[] names = { "Dhivya", "Swetha", "Viji", "Kala" };
+            return Json(new { id = 1, name = "Dhivya" });
+        }
+    }
+}
--- BeginnersApplication/Startup.cs
+++ BeginnersApplication/Startup.cs
@@ -16,22 +16,32 @@ namespace BeginnersApplication
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
+            // MVC service resgistration 
+            services.AddMvc(options=>options.EnableEndpointRouting=false);
+            //Other way of registering mVC service but need to register other services individually. This service will not bind the view with controller action.
+         //   services.AddMvcCore(options=>options.EnableEndpointRouting=false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage(); //middleware for showing the exception page for the developers.
+            }
 
+            app.UseStaticFiles();
+            //Middleware for enabling the default routing for  MVC
+            app.UseMvcWithDefaultRoute();
 
             //app.UseDefaultFiles();
             //       app.UseStaticFiles();
-            FileServerOptions fileServerOptions = new FileServerOptions();
-            fileServerOptions.DefaultFilesOptions.DefaultFileNames.Clear();
-            fileServerOptions.DefaultFilesOptions.DefaultFileNames.Add("home.html");
-            fileServerOptions.DefaultFilesOptions.DefaultFileNames.Add("home.htm");
+            //FileServerOptions fileServerOptions = new FileServerOptions();
+            //fileServerOptions.DefaultFilesOptions.DefaultFileNames.Clear();
+            //fileServerOptions.DefaultFilesOptions.DefaultFileNames.Add("home.html");
+            //fileServerOptions.DefaultFilesOptions.DefaultFileNames.Add("home.htm");
 
-            app.UseFileServer(fileServerOptions);
+            //app.UseFileServer(fileServerOptions);
 
         }
     }
